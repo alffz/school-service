d8244d8ad6b7656c72056c8a78d3a6be1b83dbe2
algoritmaAZ <dev.alfri@gmail.com>
2023-10-15 08:23:04 +0700

    register allowed resource

--- src/app/web.js
+++ src/app/web.js
@@ -18,11 +18,7 @@ web.use(cookieParser());
 web.use(express.json());
 web.use(
   cors({
-    origin: [
-      "http://localhost:3001",
-      "https://school-nx3gnczrg-alffzs-projects.vercel.app",
-      "https://school-web-git-main-alffzs-projects.vercel.app",
-    ], // allow for this url
+    origin: ["http://localhost:3001", "https://school-web-six.vercel.app"], // allow for this url
     credentials: true, //to set cookie in localstorage , but postman didn need it
   })
 );
